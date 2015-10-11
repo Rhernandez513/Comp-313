53c5769fb3cdfb606f36cd6973ebecd29fb54ba7
Thomas Brown <tbrown2@luc.edu>
2015-10-11 17:21:55 -0500

    Updated Draw.Java

--- projects/proj3/app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ projects/proj3/app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -3,6 +3,9 @@ package edu.luc.etl.cs313.android.shapes.android;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+
+import java.util.List;
+
 import edu.luc.etl.cs313.android.shapes.model.*;
 
 /**
@@ -30,44 +33,61 @@ public class Draw implements Visitor<Void> {
 
 	@Override
 	public Void onStroke(final Stroke c) {
-
+		paint.setColor(c.getColor());
+		c.getShape().accept(this);
 		return null;
 	}
 
 	@Override
 	public Void onFill(final Fill f) {
-
+		paint.setStyle(Style.FILL);
+		f.getShape().accept(this);
 		return null;
 	}
 
 	@Override
 	public Void onGroup(final Group g) {
-
+		for (Shape s: g.getShapes())
+		{s.accept(this);}
 		return null;
 	}
 
 	@Override
 	public Void onLocation(final Location l) {
-
+		canvas.save();
+		canvas.translate(l.getX(), l.getY());
+		Shape s = l.getShape();
+		s.accept(this);
+		//canvas.restore();
 		return null;
 	}
 
 	@Override
 	public Void onRectangle(final Rectangle r) {
-
+		canvas.drawRect(0,0,r.getHeight(), r.getWidth(), paint);
 		return null;
 	}
 
 	@Override
 	public Void onOutline(Outline o) {
-
+		paint.setStyle(Style.STROKE);
+		o.getShape().accept(this);
 		return null;
 	}
 
 	@Override
 	public Void onPolygon(final Polygon s) {
-
-		final float[] pts = null;
+		List<? extends Point> p = s.getPoints();
+		final float[] pts = {
+				p.get(0).getX(), p.get(0).getY(),
+				p.get(1).getX(), p.get(1).getY(),
+				p.get(1).getX(), p.get(1).getY(),
+				p.get(2).getX(), p.get(2).getY(),
+				p.get(2).getX(), p.get(2).getY(),
+				p.get(3).getX(), p.get(3).getY(),
+				p.get(3).getX(), p.get(3).getY(),
+				p.get(0).getX(), p.get(0).getY(),
+		};
 
 		canvas.drawLines(pts, paint);
 		return null;
