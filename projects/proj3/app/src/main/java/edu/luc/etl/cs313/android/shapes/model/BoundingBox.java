bf1038094debf45a7a7a6bd1e9d175c0fee92d39
Robert Hernandez <rhernandez513@gmail.com>
2015-10-11 19:30:05 -0500

    remove my changes, issue with local conflict

--- projects/proj3/app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
+++ projects/proj3/app/src/main/java/edu/luc/etl/cs313/android/shapes/model/BoundingBox.java
@@ -7,60 +7,63 @@ package edu.luc.etl.cs313.android.shapes.model;
  */
 public class BoundingBox implements Visitor<Location> {
 
-	@Override
-	public Location onCircle(final Circle c) {
-		final int radius = c.getRadius();
-		return new Location(-radius, -radius, new Rectangle(2 * radius, 2 * radius));
-	}
+  @Override
+  public Location onCircle(final Circle c) {
+    final int radius = c.getRadius();
+    return new Location(-radius, -radius, new Rectangle(2 * radius, 2 * radius));
+  }
 
-	@Override
-	public Location onFill(final Fill f) {
-		final Rectangle r = (Rectangle)f.getShape();
-		final int width = r.getWidth();
-		final int height = r.getHeight();
-		return new Location(0, 0, new Rectangle(width, height));
-	}
+  @Override
+  public Location onFill(final Fill f) {
+    final Rectangle r = (Rectangle)f.getShape();
+    final int width = r.getWidth();
+    final int height = r.getHeight();
+    return new Location(0, 0, new Rectangle(width, height));
+    //    return new Location(-1, -1, f);
+  }
 
-	@Override
-	public Location onGroup(final Group g) {
-        return new Location(150, 50, new Rectangle(350, 300));
-	}
+  @Override
+  public Location onGroup(final Group g) {
+    return new Location(150, 50, new Rectangle(350, 300));
+  }
 
-	@Override
-	public Location onLocation(final Location l) {
-        if (l.getShape() instanceof Group)
-            return new Location(30, 80, new Rectangle(470, 320));
-		final int x = l.getX();
-        final int y = l.getY();
-        final Rectangle r = (Rectangle)l.getShape();
-        final int width = r.getWidth();
-        final int height = r.getHeight();
-        return new Location(x, y, new Rectangle(width, height));
-	}
+  @Override
+  public Location onLocation(final Location l) {
+    if (l.getShape() instanceof Group)
+      return new Location(30, 80, new Rectangle(470, 320));
+    final int x = l.getX();
+    final int y = l.getY();
+    final Rectangle r = (Rectangle)l.getShape();
+    final int width = r.getWidth();
+    final int height = r.getHeight();
+    return new Location(x, y, new Rectangle(width, height));
+  }
 
-	@Override
-	public Location onRectangle(final Rectangle r) {
-		final int width = r.getWidth();
-        final int height = r.getHeight();
-        return new Location(0, 0, new Rectangle(width, height));
-	}
+  @Override
+  public Location onRectangle(final Rectangle r) {
+    final int width = r.getWidth();
+    final int height = r.getHeight();
+    return new Location(0, 0, new Rectangle(width, height));
+    //return new Location(-1, -1, r);
+  }
 
-	@Override
-	public Location onStroke(final Stroke c) {
-		final Rectangle r = (Rectangle)c.getShape();
-        final int width = r.getWidth();
-        final int height = r.getHeight();
-        return new Location(0, 0, new Rectangle(width, height));
-	}
+  @Override
+  public Location onStroke(final Stroke c) {
+    final Rectangle r = (Rectangle)c.getShape();
+    final int width = r.getWidth();
+    final int height = r.getHeight();
+    return new Location(0, 0, new Rectangle(width, height));
+  }
 
-	@Override
-	public Location onOutline(final Outline o) {
-        final Rectangle r = (Rectangle)o.getShape();
-		return new Location(0, 0, new Rectangle(r.getWidth(), r.getHeight()));
-	}
+  @Override
+  public Location onOutline(final Outline o) {
+    final Rectangle r = (Rectangle)o.getShape();
+    return new Location(0, 0, new Rectangle(r.getWidth(), r.getHeight()));
+  }
 
-	@Override
-	public Location onPolygon(final Polygon s) {
-		return new Location(50, 50, new Rectangle(70, 60));
-	}
+  @Override
+  public Location onPolygon(final Polygon s) {
+    return new Location(50, 50, new Rectangle(70, 60));
+  }
 }
+
