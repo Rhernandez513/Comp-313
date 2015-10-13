7d6187c33895638046b5dad96d28443cf4723fbb
Robert Hernandez <rhernandez513@gmail.com>
2015-10-13 18:36:13 -0500

    remove some unneeded comments & fix Visitor<Size>.onGroup(Group g)

--- projects/proj3/app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Size.java
+++ projects/proj3/app/src/main/java/edu/luc/etl/cs313/android/shapes/model/Size.java
@@ -6,16 +6,10 @@ package edu.luc.etl.cs313.android.shapes.model;
  */
 public class Size implements Visitor<Integer> {
 
-	// TODO entirely your job
+  // DONE
 
 	@Override
 	public Integer onPolygon(final Polygon p) {
-    // int count = 0;
-    // for (Point point : p.getPoints()) {
-    //   count += point.accept(this);
-    // }
-    // return count;
-
 		return 1;  // Would a polygon count as one shape?
 	}
 
@@ -26,13 +20,13 @@ public class Size implements Visitor<Integer> {
 
 	@Override
 	public Integer onGroup(final Group g) {
-		return g.getShapes().size(); // Group doesn't have a size method
+		// return g.getShapes().size();
 
-    // int count = 0;
-    // for (Shape shape : g.getShapes()) {
-    //   count += shape.accept(this);
-    // }
-    // return count;
+    int count = 0;
+    for (Shape shape : g.getShapes()) {
+      count += shape.accept(this);
+    }
+    return count;
 	}
 
 	@Override
@@ -42,28 +36,23 @@ public class Size implements Visitor<Integer> {
 
 	@Override
 	public Integer onOutline(final Outline o) {
-    // I don't think that an outline affects size but is only a decorator
 		return o.getShape().accept(this);
 	}
 
 	@Override
 	public Integer onFill(final Fill f) {
-    // Same as outline?
 		return f.getShape().accept(this);
 	}
 
 	@Override
 	public Integer onLocation(final Location l) {
-		if (!(l.getShape() instanceof Group)) { return 1; }
-      return 6; // I see what your doing but is Group limited to 6?
-
-    // Same thing as outline?
-		// return l.getShape().accept(this);
+		// if (!(l.getShape() instanceof Group)) { return 1; }
+    // return 6; // I see what your doing but is Group limited to 6?
+    return l.getShape().accept(this);
 	}
 
 	@Override
 	public Integer onStroke(final Stroke c) {
-    // Same thing as outline & location?
 		return c.getShape().accept(this);
 	}
 }
